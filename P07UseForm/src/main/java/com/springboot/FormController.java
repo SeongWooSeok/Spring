f8a32af114a8446969b350f1b22fa5fbafe300dc
DESKTOP-1SPG7AQ\kosmo <tjddntjr98@naver.com>
2023-04-25 18:05:28 +0900

    no message

---
+++ P07UseForm/src/main/java/com/springboot/FormController.java
@@ -0,0 +1,49 @@
+package com.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class FormController {
+	
+	@RequestMapping("/")
+	public String root() {
+		return "form";
+	}
+	
+	@RequestMapping("/test1")
+	public Model test1(HttpServletRequest httpServletRequest, Model model) {
+		String age= httpServletRequest.getParameter("age");
+		String name= httpServletRequest.getParameter("name");
+		
+		model.addAttribute("age", age);
+		model.addAttribute("name", name);
+		return model;
+	}
+	
+	@RequestMapping("/test2")
+	public String test2(@RequestParam("age") String age, @RequestParam("name") String name, Model model) {
+		
+		model.addAttribute("age", age);
+		model.addAttribute("name", name);
+		return "test1";
+	}
+	
+	@RequestMapping("/test3/{personname}/{personage}")
+	public String getPathInfo(@PathVariable String personname, @PathVariable String personage, Model model) {
+		model.addAttribute("age", personage);
+		model.addAttribute("name", personname);
+		return "test1";
+	}
+	
+	@RequestMapping("/test4")
+	public String test4(PersonDTO person, Model model) {
+		model.addAttribute("person", person);
+		return "test1";
+	}
+}
---
+++ P08Lombok/src/main/java/com/springboot/PersonDTO.java
@@ -0,0 +1,12 @@
+package com.springboot;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonDTO {
+	private String name;
+	private String age;
+
+}
